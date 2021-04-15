23609d77bc1f72b625da5b1eedc2ca6263baf837
Estefania <estefania.caceres@enevasys.com>
2021-04-15 16:41:57 -0500

    upload before class

--- lib/pages/application/upload.dart
+++ lib/pages/application/upload.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:gallery_array/localization/constants.dart';
 import 'package:gallery_array/pages/shared/app_bar.dart';
 import 'package:gallery_array/pages/shared/drawer.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class UploadPage extends StatefulWidget {
   @override
@@ -9,18 +14,74 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  String imageUrl;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: CommonAppBar(title: getTransValue(context, 'upload-photo'),appBar: AppBar()),
         drawer: DrawerList(),
         body: Container(
-        child: Column(
-          children: [
-            Text('Photo')
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text('Photo'),
+              Spacer(),
+              ElevatedButton(
+                onPressed: (){
+                  uploadImage();
+                },
+                child: Text(
+                  getTransValue(context, 'upload-file'),
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF7B39ED),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  uploadImage() async {
+    final _storage = FirebaseStorage.instance;
+    final _picker = ImagePicker();
+    PickedFile image;
+
+    //Check Permissions
+    await Permission.photos.request();
+
+    var permissionStatus = await Permission.photos.status;
+
+    if (permissionStatus.isGranted){
+      //Select Image
+      image = await _picker.getImage(source: ImageSource.gallery);
+      var file = File(image.path);
+
+      if (image != null){
+        //Upload to Firebase
+        var snapshot = await _storage.ref()
+            .child('folderName/imageName')
+            .putFile(file);
+
+        var downloadUrl = await snapshot.ref.getDownloadURL();
+
+        setState(() {
+          imageUrl = downloadUrl;
+        });
+      } else {
+        print('No Path Received');
+      }
+
+    } else {
+      print('Grant Permissions and try again');
+    }
+
+
+
+
   }
 }
